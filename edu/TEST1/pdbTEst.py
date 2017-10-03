8612cfc25258756f90f265ea700eed1e4de9ae58
zeagan <zeagan2@gmail.com>
2017-10-02 23:07:15 -0700

    Comments and reading some docs

--- edu/TEST1/pdbTEst.py
+++ edu/TEST1/pdbTEst.py
@@ -3,28 +3,33 @@
 
 import vtk
 
-filename = "test.pdb"
+filename = "file_pdb/cafeine.pdb" #file path for input 
 
 
 #Declare objects
-ren = vtk.vtkRenderer()
-renWin = vtk.vtkRenderWindow()
-iren = vtk.vtkRenderWindowInteractor()
-sphere = vtk.vtkSphereSource()
-pdbReader = vtk.vtkPDBReader()
+ren = vtk.vtkRenderer() #link to the graphics pipeline
+renWin = vtk.vtkRenderWindow() #window to hold graphics stuff
+iren = vtk.vtkRenderWindowInteractor() #adds basic human UI to window
+sphere = vtk.vtkSphereSource() #sphere to test window with
+#found an example using this pdb reader
+#documentations kinda sucky but might work
+#def don't want to parse pdb format by hand, not enough time
+#similar to improvement of pcap over fscanf
+pdbReader = vtk.vtkPDBReader() 
+#the examples also all use this thing. no idea what it is but it
+#seems to convert from shape primitives to somthing displayable
 mapp = vtk.vtkPolyDataMapper()
 
 
-
 # Setup FileIO
-pdbReader.SetFileName("cafeine.pdb");
+pdbReader.SetFileName(filename); 
+renWin.
+
+
+
+
 
 iren.SetRenderWindow(renWin)
-
-
-
-
-
 # Enable user interface interactor
 iren.Initialize()
 renWin.Render()
