c4e827fb01686dfb67519f072ba7c1fdbb2cc1d5
Zachary <zeagan2@gmail.com>
2017-10-24 12:24:35 -0700

    started lab4

---
+++ lab4/prob1/cube.py
@@ -0,0 +1,22 @@
+import vtk
+
+ren = vtk.vtkRenderer()
+renwin = vtk.vtkRenderWindow()
+iren = vtk.vtkInteractor()
+
+
+#points = vtk.vtkPoints()
+#topo = vtk.vtkCellArray()
+#cube = vtk.vtkPolyData()
+
+#cube.SetPoints()
+#cube.SetVerts()
+#cube.SetLines()
+#cube.SetPolys()
+#cube.SetStrips()
+
+#pd = cube.GetCellData() #retrieve cell data
+#pd = cube.GetPointData() #retrieve point data
+
+#pd.SetScalars()
+
