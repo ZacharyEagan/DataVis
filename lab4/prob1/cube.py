ec294f20e4a77004d01df4593511923fda721a21
zeagan <zeagan2@gmail.com>
2017-10-24 17:33:06 -0700

    Completed Lab4 part 1

--- lab4/prob1/cube.py
+++ lab4/prob1/cube.py
@@ -1,11 +1,66 @@
 import vtk
 
-ren = vtk.vtkRenderer()
 renwin = vtk.vtkRenderWindow()
-iren = vtk.vtkInteractor()
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renwin)
+ren = vtk.vtkRenderer()
 
 
-#points = vtk.vtkPoints()
+
+
+
+
+points = vtk.vtkPoints()
+points.InsertNextPoint(0.0,0.0,0.0) #0 0 
+points.InsertNextPoint(0.0,0.0,1.0) #1 Z
+points.InsertNextPoint(0.0,1.0,0.0) #2 Y
+points.InsertNextPoint(0.0,1.0,1.0) #3 YZ
+points.InsertNextPoint(1.0,0.0,0.0) #4 X
+points.InsertNextPoint(1.0,0.0,1.0) #5 XZ
+points.InsertNextPoint(1.0,1.0,0.0) #6 XY
+points.InsertNextPoint(1.0,1.0,1.0) #7 XYZ
+
+
+cells = vtk.vtkCellArray()
+cells.InsertNextCell(4, [0, 4, 6, 2])
+cells.InsertNextCell(4, [0, 2, 3, 1])
+cells.InsertNextCell(4, [0, 1, 5, 4])
+cells.InsertNextCell(4, [2, 6, 7, 3])
+cells.InsertNextCell(4, [4, 5, 7, 6])
+cells.InsertNextCell(4, [5, 1, 3, 7])
+
+scalars = vtk.vtkFloatArray()
+for i in range (8):
+   scalars.InsertTuple(i, (i,))
+
+
+cube = vtk.vtkPolyData()
+cube.SetPoints(points)
+cube.SetPolys(cells)
+cube.GetPointData().SetScalars(scalars)
+
+
+
+#cube.SetVerts(cells)
+#cube.SetLines(cells)
+
+
+
+#cube.SetVerts()
+
+
+mapp = vtk.vtkPolyDataMapper()
+mapp.SetInputData(cube)
+mapp.SetScalarRange(0,8)
+actor = vtk.vtkActor()
+actor.SetMapper(mapp)
+ren.AddActor(actor)
+renwin.AddRenderer(ren)
+iren.Initialize()
+renwin.Render()
+iren.Start()
+
+
 #topo = vtk.vtkCellArray()
 #cube = vtk.vtkPolyData()
 
