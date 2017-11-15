7320198d9faa6f69cf8f7154542ef9feb6c1c69b
zeagan <zeagan2@gmail.com>
2017-11-14 22:10:40 -0800

    First confirmed interaction with blender, added spheres

--- lab6/fishIris.py
+++ lab6/fishIris.py
@@ -3,6 +3,8 @@
 
 #I wont be coppying directly from the site, rather I will use it as a reference when I am stuck without instruction on how to proceded. However since the requirements for this project are in essence "copy this site" the results will likely be very very similar.  
 
+import bpy  #blender python api
+
 sepal_length = []
 sepal_width = []
 petal_length = []
@@ -15,10 +17,12 @@ with  open("iris.csv", "r") as iris_file:
 #      print len(data)
       if len(data) == 5:
 #      print data
-         sepal_length.append(data[0])
-         sepal_width.append(data[1])
-         petal_length.append(data[2])  
-         petal_width.append(data[3])
+         sepal_length.append(float(data[0]))
+         sepal_width.append(float(data[1]))
+         petal_length.append(float(data[2]))
+         petal_width.append(float(data[3]))
          iris_species.append(data[4])
-   for flower in iris_species:
-      print (flower)
+for i in range(len(iris_species)):
+   bpy.ops.mesh.primitive_uv_sphere_add(location=(sepal_length[i], sepal_width[i], petal_length[i]))
+   bpy.context.object.name = iris_species[i] + "-at_row-" + str(i)
+
