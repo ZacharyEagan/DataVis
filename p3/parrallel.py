481d083346528cd5a14d733489ab3ec3211abc7f
zeagan <zeagan2@gmail.com>
2017-11-26 21:22:48 -0800

    cleanup

--- p3/parrallel.py
+++ p3/parrallel.py
@@ -1,43 +1,5 @@
 import vtk
 
-
-
-########### Iris data exctraction from lab6
-sepal_length = []
-sepal_width = []
-petal_length = []
-petal_width = []
-iris_species = []
-
-with  open("iris.csv", "r") as iris_file:
-   for line in iris_file:
-      data = line.split(",")
-#      print len(data)
-      if len(data) == 5:
-#      print data
-         sepal_length.append(float(data[0]))
-         sepal_width.append(float(data[1]))
-         petal_length.append(float(data[2]))
-         petal_width.append(float(data[3]))
-         iris_species.append(data[4])
-
-width_sum = 0;
-for petal in petal_width:
-   width_sum += petal;
-width_av = width_sum / len(petal_width)
-
-petal_dict = {}
-for iris in iris_species:
-   petal_dict[iris] = petal_dict.get(iris,0) + 1
-print (petal_dict)
-
-
-scale = 0.01
-################end Iris Data Extraction
-
-
-
-
 reader = vtk.vtkDelimitedTextReader()
 reader.DetectNumericColumnsOn()
 reader.SetFieldDelimiterCharacters(",")
@@ -58,12 +20,6 @@ view.GetRenderWindow().SetSize(600,300)
 chart = vtk.vtkChartParallelCoordinates()
 view.GetScene().AddItem(chart)
 
-print chart.GetPlot(0)
-print " "
-print type(chart.GetPlot(0))
-print " "
-
-
 chart.GetPlot(0).SetInputData(reader.GetOutput())
 
 view.ResetCamera()
@@ -72,36 +28,3 @@ view.Render()
 # Start interaction event loop
 view.GetInteractor().Start()
 
-
-
-
-
-
-
-#parRep = vtk.vtkParallelCoordinatesRepresentation()
-#parRep.SetInputConnection(data.GetOutputPort())
-#parRep.SetInputConnection(reader.GetOutputPort())
-#parRep.SetInputArrayToProcess(0,0,0,0,'petal')
-
-#parView = vtk.vtkParallelCoordinatesView()
-#parView.SetRepresentation(parRep)
-#parView.SetInspectMode(1)
-#parView.SetBrushOperatorToReplace()
-#parView.SetBrushModeToLasso()
-
-
-
-#ren = vtk.vtkRenderer()
-#ren.AddActor()
-#renwin = vtk.vtkRenderWindow()
-#renwin.AddRenderer(ren)
-#iren = vtk.vtkRenderWindowInteractor()
-#iren.SetRenderWindow(renwin)
-#renwin.Render()
-
-#parView.GetRenderWindow().SetSize(600,300)
-#parView.ResetCamera()
-#parView.Render()
-#parView.GetInteractor().Start()
-
-#iren.Start()
