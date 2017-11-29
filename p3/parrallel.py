558cb40fd060374e111fd789675cd90343aac2d2
zeagan <zeagan2@gmail.com>
2017-11-29 14:08:17 -0800

    starting pca space

--- p3/parrallel.py
+++ p3/parrallel.py
@@ -5,14 +5,18 @@ reader.DetectNumericColumnsOn()
 reader.SetFieldDelimiterCharacters(",")
 reader.SetHaveHeaders(True)
 #reader.SetHaveHeaders(False)
-reader.SetMaxRecords(400)
+reader.SetMaxRecords(20)
 #print reader.GetHeaders()
-reader.SetFileName("iris.csv")
+#reader.SetFileName("iris.csv")
+reader.SetFileName("postscndryunivsrvy2013dirinfo.csv")
 reader.Update()
+
+print "data read"
 
 rep = vtk.vtkParallelCoordinatesRepresentation()
 rep.SetInputConnection(reader.GetOutputPort())
-
+rep.SetUseCurves(1)
+#rep.SelectColorArray(1)
 
 view = vtk.vtkContextView()
 view.GetRenderer().SetBackground(1.0, 1.0, 1.0)
@@ -21,8 +25,8 @@ view.GetRenderWindow().SetSize(600,300)
 chart = vtk.vtkChartParallelCoordinates()
 view.GetScene().AddItem(chart)
 
-chart.GetPlot(0).SetInputData(reader.GetOutput())
 
+chart.GetPlot(0).SetInputData(reader.GetOutput())
 view.ResetCamera()
 view.Render()
 
